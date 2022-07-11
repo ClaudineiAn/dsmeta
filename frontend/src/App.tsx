7656321a5221e2e49c6a801a4d72ebc852ad4050
Claudinei Aparecido Angelo <claudinei2092016@gmail.com>
2022-07-11 16:56:49 -0300

    Datepicker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
   return (
@@ -12,10 +14,20 @@ function App() {
                     <h2 className="dsmeta-sales-title">Vendas</h2>
                     <div>
                         <div className="dsmeta-form-control-container">
-                            <input className="dsmeta-form-control" type="text" />
+                            <DatePicker
+                                selected={new Date()}
+                                onChange={(date: Date) => {}}
+                                className="dsmeta-form-control"
+                                dateFormat="dd/MM/yyyy"
+                            />
                         </div>
                         <div className="dsmeta-form-control-container">
-                            <input className="dsmeta-form-control" type="text" />
+                            <DatePicker
+                                selected={new Date()}
+                                onChange={(date: Date) => {}}
+                                className="dsmeta-form-control"
+                                dateFormat="dd/MM/yyyy"
+                            />
                         </div>
                     </div>
                     <div>
